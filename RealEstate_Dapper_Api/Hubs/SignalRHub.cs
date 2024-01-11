a7d17f12ea618ec5b632e72194bf8ce07fa8eb33
Mert Gündoğdu <mertgnd36@gmail.com>
2024-01-11 14:55:52 +0100

    SignalR configuration and first value sent.

--- RealEstate_Dapper_Api/Hubs/SignalRHub.cs
+++ RealEstate_Dapper_Api/Hubs/SignalRHub.cs
@@ -4,5 +4,19 @@ namespace RealEstate_Dapper_Api.Hubs
 {
     public class SignalRHub : Hub
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public SignalRHub(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task SendCategoryCount()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44366/api/Statistics/CategoryCount");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            await Clients.All.SendAsync("ReceiveCategoryCount", jsonData);
+        }
     }
 }
--- RealEstate_Dapper_Api/Program.cs
+++ RealEstate_Dapper_Api/Program.cs
@@ -38,7 +38,7 @@ builder.Services.AddCors(opt =>
                .AllowCredentials();
     });
 });
-
+builder.Services.AddHttpClient();
 builder.Services.AddSignalR();
 
 builder.Services.AddControllers();
